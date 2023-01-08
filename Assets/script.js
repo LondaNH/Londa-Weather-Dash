aeefdf9682d7ca842a38d724cc39ec1fbfe0e158
LondaNH <117847738+LondaNH@users.noreply.github.com>
2023-01-08 15:23:24 -0500

    updated readme and js file

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,8 @@
-// Ensures document is loaded, and only loads once. 
+// Only loads doc once. 
 $(document).ready(function () {
    function onLoad(){
    }
-   // This is the function to get search history from local storage. 
+   //  Function to get search history from local storage. 
    const history = JSON.parse(localStorage.getItem('search-history')) || [];
    const historyLastValue = history[history.length - 1];
    console.log(historyLastValue);
